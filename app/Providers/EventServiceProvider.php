d57c0e2c535627337ae15c6932a8efd70fe0c242
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 18:23:29 +0000

    registered bidcreated and process autobiding listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+
+use App\Events\BidCreated;
+use App\Listeners\ProcessAutoBidding;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,8 +16,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        BidCreated::class => [
+            ProcessAutoBidding::class,
         ],
     ];
 
