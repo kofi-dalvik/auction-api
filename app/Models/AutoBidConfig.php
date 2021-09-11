fa3bfcc2eea76b0e49650a9a02658fa9d122c490
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 13:15:02 +0000

    added auto  bid  config model

---
+++ app/Models/AutoBidConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Relationships\UserTrait;
+
+class AutoBidConfig extends Model
+{
+    use HasFactory;
+}
