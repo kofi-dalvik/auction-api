294f7201ee9a17a59f9af4b2ea86418391227525
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 17:12:30 +0000

    added  enhancements and fixes

--- app/Actions/ListItemAction.php
+++ app/Actions/ListItemAction.php
@@ -2,9 +2,10 @@
 
 namespace App\Actions;
 
+use Arr;
 use App\Models\Item;
-use Illuminate\Pagination\Paginator;
-use App\Querries\KeywordSearchTrait;
+use Illuminate\Pagination\LengthAwarePaginator;
+use App\Queries\KeywordSearchTrait;
 
 class ListItemAction
 {
@@ -16,17 +17,19 @@ class ListItemAction
      * Get items listings
      *
      * @param array $params
-     * @return \Illuminate\Pagination\Paginator
+     * @return \Illuminate\Pagination\LengthAwarePaginator
      */
-    public function execute(array $params): Paginator
+    public function execute(array $params): LengthAwarePaginator
     {
-        $sort_price  = strtolower($params['sort_price']);
+        $sort_price  = strtolower(Arr::get($params, 'sort_price', 'asc'));
         $sort_by_price = in_array($sort_price, $this->sort_directions);
+        $keyword = Arr::get($params, 'keyword', null);
 
         $query = Item::with('images')
                     ->when($sort_by_price, fn ($query) => $query->orderBy('price', $sort_price))
                     ->when($keyword, fn () => $this->keywordSearch($query, $keyword, ['name', 'description']))
-                    ->latest('created_at')
-                    ->paginate(10);
+                    ->latest('created_at');
+
+        return $query->paginate(10);
     }
 }
