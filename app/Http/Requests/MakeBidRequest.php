0cece23e9069cbdc417393f2dea1850c89300ad4
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 19:48:58 +0000

    added make bid form request

---
+++ app/Http/Requests/MakeBidRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class MakeBidRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'item_id' => [
+                'required',
+                Rule::exists('items', 'id')->where(fn ($query) => $query->where('closing_date', '>', now()))
+            ],
+            'amount' => 'required|numeric'
+        ];
+    }
+}
