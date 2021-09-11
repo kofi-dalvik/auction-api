b360c07b3a1c2ea14ce9ecc43d6f393aca3cde05
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 19:49:43 +0000

    changed aftermethod

--- database/migrations/2021_09_11_150035_add_latest_bid_id_to_items_table.php
+++ database/migrations/2021_09_11_150035_add_latest_bid_id_to_items_table.php
@@ -14,7 +14,7 @@ class AddLatestBidIdToItemsTable extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->foreignId('latest_bid_id')->after('active')->nullable();
+            $table->foreignId('latest_bid_id')->after('closing_date')->nullable();
 
             $table->foreign('latest_bid_id')
                 ->references('id')
