062c3961768d3a1e847c0ab1132b89332c3c08a0
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 23:12:01 +0000

    added make bids

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ItemsController;
+use App\Http\Controllers\BiddingsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,5 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/items', [ItemsController::class, 'index']);
     Route::get('/items/{id}', [ItemsController::class, 'show']);
+    Route::post('/biddings', [BiddingsController::class, 'store']);
 });
