83c6e36a8691bed2035b27d64bd613f5be5a502c
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 15:16:03 +0000

    added item factory

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Item;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ItemFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Item::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'price' => random_int(1, 150),
+            'closing_date' => $this->faker->dateTimeBetween('+1 hours', "1 months")
+        ];
+    }
+}
