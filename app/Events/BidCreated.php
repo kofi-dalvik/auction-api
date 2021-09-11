ab7ab24e9fdbb0d089c69dcb280088be152d7e50
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 18:24:15 +0000

    added bid created event and channel to broadcast on

---
+++ app/Events/BidCreated.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\Bidding;
+
+class BidCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $bidding;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Bidding $bidding)
+    {
+        $this->bidding = $bidding;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('item.' . $this->bidding->id . '.bidding');
+    }
+}
