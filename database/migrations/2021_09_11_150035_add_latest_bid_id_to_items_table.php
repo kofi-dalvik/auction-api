6a131e832d863025a739262f387cf74dc156309a
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-11 15:16:37 +0000

    added latest bid id to items table

---
+++ database/migrations/2021_09_11_150035_add_latest_bid_id_to_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLatestBidIdToItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->foreignId('latest_bid_id')->nullable();
+
+            $table->foreign('latest_bid_id')
+                ->references('id')
+                ->on('biddings')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropForeign(['latest_bid_id']);
+            $table->dropColumn(['latest_bid_id']);
+        });
+    }
+}
