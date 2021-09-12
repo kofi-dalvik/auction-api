25c8b76c8117f39d0cb90dfa1cc1dac8e3abbd5b
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 22:51:16 +0000

    added url  append

--- app/Models/ItemImage.php
+++ app/Models/ItemImage.php
@@ -8,4 +8,11 @@ use App\Models\Relationships\ItemTrait;
 class ItemImage extends Model
 {
     use HasFactory, ItemTrait;
+
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute()
+    {
+        return asset('images/items/' . $this->url);
+    }
 }
