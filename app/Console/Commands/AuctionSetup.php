45be391fcc6a923ae70099ae20f95bcdf9ebc497
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-13 11:17:24 +0000

    added  single command to setup the server

---
+++ app/Console/Commands/AuctionSetup.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AuctionSetup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'auction:setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Automates setting up the backend for use:
+        create database.sqlite,
+        migrate tables,
+        seed test users,
+        extract item images';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        file_put_contents(database_path('database.sqlite'), null);
+        $this->call('migrate:refresh');
+        $this->call('db:seed');
+        return 0;
+    }
+}
